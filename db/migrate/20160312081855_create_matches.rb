845068b4796ded27fa718227ade73295cc9b31c5
Rajesh Patra <p7.rajesh@gmail.com>
2016-03-12 15:29:09 +0530

    minor changes

--- db/migrate/20160312081855_create_matches.rb
+++ db/migrate/20160312081855_create_matches.rb
@@ -1,7 +1,8 @@
 class CreateMatches < ActiveRecord::Migration
   def change
     create_table :matches do |t|
-
+    	t.string :team_a
+    	t.string :team_b
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160312053207) do
+ActiveRecord::Schema.define(version: 20160312081855) do
+
+  create_table "matches", force: :cascade do |t|
+    t.string   "team_a"
+    t.string   "team_b"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tosses", force: :cascade do |t|
     t.string   "team_a"
