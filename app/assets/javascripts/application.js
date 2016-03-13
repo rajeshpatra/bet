3558eface08ba9f8ed896d2ee27bf08fd6396fa8
Rajesh Patra <p7.rajesh@gmail.com>
2016-03-13 22:58:23 +0530

    trying js code to disable radio button form field

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,37 @@
 //= require_tree .
 
 
+
+
+jQuery(document).ready(function(){
+    // $('#teama, #teamb').hide();
+    jQuery('#teama, #teamb').live('click', function(event) {        
+        jQuery('#f1').toggle('show');
+        jQuery('#f2').toggle('show');            
+    });
+});
+
+
+// $(document).ready(function () {
+//     $("#event_free").click(function () {
+//         if ($(this).is(":checked")) {
+//             $("#event_price").prop("disabled", true);
+//         } else {
+//             $("#event_price").prop("disabled", false);
+//         }
+//     });
+// });
+
+// $(function(){
+//     $("#teama, #teamb").change(function(){
+//         $("#f1, #f2").val("").attr("readonly",true);
+//         if($("#teama").is(":checked")){
+//             $("#f1").removeAttr("disabled");
+//             $("#f1").focus();
+//         }
+//         else if($("#teamb").is(":checked")){
+//             $("#f2").removeAttr("disabled");
+//             $("#f2").focus();   
+//         }
+//     });
+// });
